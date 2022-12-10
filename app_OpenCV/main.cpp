bc8abb2001b7c080996438a0ade238161cde0549
Marco <marcomenchetti96@gmail.com>
2022-12-10 18:37:44 +0100

    Forgotten dependency
    Issue GH-2

--- app_OpenCV/main.cpp
+++ app_OpenCV/main.cpp
@@ -4,7 +4,7 @@ using namespace cv;
 
 int main() {
     Mat image;
-    image = imread("sample.jpeg");
+    image = imread("../sample.jpeg");
     namedWindow("Display Image", WINDOW_AUTOSIZE);
     imshow("Display Image", image);
     waitKey(0);
